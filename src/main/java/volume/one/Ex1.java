4b2ca238a2a8796e3d2d7446e152f2ebad3cc307
Galq <galina.sansarowa@gmail.com>
2020-12-17 17:24:58 +0200

    complete exercise one

---
+++ src/main/java/volume/one/Ex1.java
@@ -0,0 +1,12 @@
+package volume.one;
+
+public class Ex1 {
+    static  int c;
+    static char v;
+    public static void main(String[] args) {
+
+
+        System.out.println("c " + c + " v" + v);
+    }
+
+}
