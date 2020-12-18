6a15ff31f48413cff714653176127ff2ab6ace08
Galq <galina.sansarowa@gmail.com>
2020-12-18 17:43:40 +0200

    complete exercise nine

---
+++ src/main/java/volume/one/ExerciseNine.java
@@ -0,0 +1,16 @@
+package volume.one;
+//Write a program that demonstrates that autoboxing works for all the primitive types and their wrappers.
+public class ExerciseNine {
+    public static void main(String[] args) {
+        Boolean bl= true;
+        Character ch='a';
+        Byte b = 15;
+        Short sh=100;
+        Integer i=1000;
+        Long l=111l;
+        Float f = 5.5f;
+        Double d=5.56554;
+        Void v= null;
+
+    }
+}
