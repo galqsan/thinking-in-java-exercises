ab576021f44f18883f75f871b40e6202c9ac2d69
Galq <galina.sansarowa@gmail.com>
2020-12-19 20:46:46 +0200

    complete exercise ten

---
+++ src/main/java/volume/one/ExerciseTen.java
@@ -0,0 +1,9 @@
+package volume.one;
+//Write a program that prints three arguments taken from the command line.
+// To do this, you’ll need to index into the command-line array of Strings.
+public class ExerciseTen {
+    public static void main(String...args) {
+        System.out.println("First index:"+args[0]+"\nSecond index:"+args[1]+"\nThirt index:"+args[2]);
+
+    }
+}
