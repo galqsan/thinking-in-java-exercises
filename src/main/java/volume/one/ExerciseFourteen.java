454948045ca191e0188050c27b1ef97706ad2b9c
Galq <galina.sansarowa@gmail.com>
2020-12-21 21:58:41 +0200

    complete exercise fourteen

---
+++ src/main/java/volume/one/ExerciseFourteen.java
@@ -0,0 +1,52 @@
+package volume.one;
+//Add an HTML list of items to the documentation in the previous exercise.
+
+/**
+ * List :
+ * <ol>
+ * <li>  one
+ * <li>  two
+ * <li> three
+ * </ol>
+ */
+public class ExerciseFourteen {
+    /**
+     * List of item:
+     * <ol>
+     * <li> Item one
+     * <li> Item two
+     * <li> Item three
+     * </ol>
+     */
+    public int f;
+    /**
+     * List in public int :
+     * <ol>
+     * <li>  one
+     * <li>  two
+     * <li>  three
+     * </ol>
+     */
+    private int p;
+
+    /**
+     * List in private int :
+     * <ol>
+     * <li>  one
+     * <li>  two
+     * <li> three
+     * </ol>
+     */
+    public static void main(String[] args) {
+        /**
+         *  List in main:
+         * <ol>
+         * <li> Item one
+         * <li> Item two
+         * <li> Item three
+         * </ol>
+         */
+    }
+
+
+}
