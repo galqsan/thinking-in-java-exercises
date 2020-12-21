2aa8d7a3d9feca91e5aa2790535fcb3685c73dd0
Galq <galina.sansarowa@gmail.com>
2020-12-21 22:00:55 +0200

    complete exercise fifteen

---
+++ src/main/java/volume/one/ExerciseFifteen.java
@@ -0,0 +1,17 @@
+package volume.one;
+
+//Take the program in Exercise 2 and add comment documentation to it.
+//Extract this comment documentation into an HTML file using Javadoc and view it with your Web browser.
+
+/**
+ * Test the documentation
+ * @author Galq
+ * @version "1.8.0"
+ */
+public class ExerciseFifteen {
+    public static void main(String[] args) {
+
+        System.out.println(" Hello world");
+    }
+
+}
