f21d036397964cf43413be9c5f2ba4b7b66bfcb1
Galq <galina.sansarowa@gmail.com>
2021-02-25 22:42:40 +0200

    complete exercise four

---
+++ src/main/java/volume/two/ExerciseFour.java
@@ -0,0 +1,11 @@
+package volume.two;
+//Write a program that calculates velocity using a constant distance and a constant time.
+public class ExerciseFour {
+
+    public static void main(String[] args) {
+        double distance =65;
+        double time=30;
+        double velocity=distance/time;
+        System.out.println("velocity "+velocity+" using  distance "+distance+" and  time "+time);
+    }
+}
