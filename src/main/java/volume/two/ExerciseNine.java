7da23b68761382f38ab762250e5b75b6d8a29dba
Galq <galina.sansarowa@gmail.com>
2021-03-05 21:27:04 +0200

    complete exercise nine

---
+++ src/main/java/volume/two/ExerciseNine.java
@@ -0,0 +1,14 @@
+package volume.two;
+//Exercise 9: Display the largest and smallest numbers for both float and double exponential notation.
+public class ExerciseNine {
+    public static void main(String[] args) {
+        float flMax = Float.MAX_VALUE;
+        float flMin =Float.MIN_VALUE;
+        double doMax=Double.MAX_VALUE;
+        double doMin= Double.MIN_VALUE;
+        System.out.println("largest fload number is "+flMax+ " smallest fload number is "+flMin);
+        System.out.println("largest double number is "+doMax+ " smallest double number is "+doMin);
+
+    }
+}
+
