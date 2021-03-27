378ba699ab4b6c5793b173873235a9c7c815f7c5
Galq <galina.sansarowa@gmail.com>
2021-03-27 22:05:43 +0200

    complete exercise one

---
+++ src/main/java/volume/three/ExerciseOne.java
@@ -0,0 +1,9 @@
+package volume.three;
+//Exercise 1:Write a program that prints values from 1 to 100.
+public class ExerciseOne {
+    public static void main(String[] args) {
+        for (int i=1;i<=100;i++) {
+            System.out.println("interation number: " + i);
+        }
+    }
+}
