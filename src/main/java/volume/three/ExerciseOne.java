9bc4ea9a46cd54239c7c3ed6ccf83db18c024029
Galq <galina.sansarowa@gmail.com>
2021-06-08 14:52:44 +0300

    complete exercise seven

--- src/main/java/volume/three/ExerciseOne.java
+++ src/main/java/volume/three/ExerciseOne.java
@@ -1,9 +1,18 @@
 package volume.three;
 //Exercise 1:Write a program that prints values from 1 to 100.
+//Exercise 7:Modify Exercise 1 so that the program exits by using the break keyword at value 99.
+// Try using return instead.
 public class ExerciseOne {
     public static void main(String[] args) {
         for (int i=1;i<=100;i++) {
-            System.out.println("interation number: " + i);
+            System.out.println("interation number with break: " + i);
+            if(i==99)
+                break;
+        }
+        for (int i=1;i<=100;i++) {
+            System.out.println("interation number with return: " + i);
+            if(i==99)
+                return;
         }
     }
 }
