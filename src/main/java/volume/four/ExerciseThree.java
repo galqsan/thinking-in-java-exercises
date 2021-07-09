e330d9c206896bb7eb13822f38cf1278e1ed4100
Galq <galina.sansarowa@gmail.com>
2021-07-09 13:25:17 +0300

    complete exercise three

---
+++ src/main/java/volume/four/ExerciseThree.java
@@ -0,0 +1,11 @@
+package volume.four;
+//Exercise 3:Create a class with a default constructor (one that takes no arguments) that prints a message.
+// Create an object of this class.
+public class ExerciseThree {
+    ExerciseThree(){
+        System.out.println("Constructor");
+    }
+    public static void main(String[] args) {
+        new ExerciseThree();
+    }
+}
