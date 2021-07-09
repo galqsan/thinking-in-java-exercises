d51e5e7479d6026506b0c7546ad519f0f2ab50e8
Galq <galina.sansarowa@gmail.com>
2021-07-09 15:42:23 +0300

    complete exercise six

--- src/main/java/volume/four/Dog.java
+++ src/main/java/volume/four/Dog.java
@@ -2,6 +2,9 @@ package volume.four;
 //Exercise 5:Create a class called Dog with an overloaded bark( ) method.
 // This method should be overloaded based on various primitive data types, and print different types of barking, howling, etc., depending on which overloaded version is called.
 // Write a main( ) that calls all the different versions.
+//Exercise 6:Modify the previous exercise so that two of the overloaded methods have two arguments (of two different types)
+// , but in reversed order relative to each other.
+// Verify that this works.
 public class Dog {
     void bark(char a){
         System.out.println("bau");
@@ -24,11 +27,12 @@ public class Dog {
     void bark(double d){
         System.out.println("grr");
     }
-    void bark(String name){
-        System.out.println(" bark");
+    void bark(String name,int i){
+        System.out.println(" bzz");
     }
-
-
+    void bark(int i,String name){
+        System.out.println(" meow");
+    }
     public static void main(String[] args) {
         Dog dog=new Dog();
         int i=2;
@@ -40,7 +44,8 @@ public class Dog {
         dog.bark(3.5f);
         dog.bark(8.1);
         dog.bark(5);
-        dog.bark("Boby");
+        dog.bark("Boby",5);
+        dog.bark(12,"jaz");
     }
 
 }
