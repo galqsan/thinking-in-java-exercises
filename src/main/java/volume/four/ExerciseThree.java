468e0a968259c209bdb13ab1bd3f4729c1e61175
Galq <galina.sansarowa@gmail.com>
2021-07-09 14:59:09 +0300

    complete exercise four

--- src/main/java/volume/four/ExerciseThree.java
+++ src/main/java/volume/four/ExerciseThree.java
@@ -1,11 +1,17 @@
 package volume.four;
 //Exercise 3:Create a class with a default constructor (one that takes no arguments) that prints a message.
 // Create an object of this class.
+//Exercise 4:Add an overloaded constructor to the previous exercise that takes a String argument and prints it along with your message.
 public class ExerciseThree {
     ExerciseThree(){
         System.out.println("Constructor");
     }
+    ExerciseThree(String s){
+        System.out.println(s+" :Overloaded constructor ");
+    }
+
     public static void main(String[] args) {
-        new ExerciseThree();
+       new ExerciseThree();
+       new ExerciseThree("Exercise four");
     }
 }
