19f1b8e9fed707fde6be5026c023c1b3cb38b3c4
Galq <galina.sansarowa@gmail.com>
2021-07-24 22:59:12 +0300

    complete exercise eleven

--- src/main/java/volume/four/ExerciseTen.java
+++ src/main/java/volume/four/ExerciseTen.java
@@ -1,6 +1,7 @@
 package volume.four;
 //Exercise 10:Create a class with a finalize( ) method that prints a message.
 // In main( ), create an object of your class. Explain the behavior of your program.
+//Exercise 11:Modify the previous exercise so that your finalize( ) will always be called.
 public class ExerciseTen {
     protected void finalize(){
         System.out.println("finalize metod");
@@ -8,6 +9,7 @@ public class ExerciseTen {
 
     public static void main(String[] args) {
         ExerciseTen ten = new ExerciseTen();
+        ten.finalize();
     }
 
 }
