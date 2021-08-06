f6554177b5298af0defe0e4d1d0187a49392b819
Galq <galina.sansarowa@gmail.com>
2021-08-06 21:34:09 +0300

    complete exercise eighteen from volume four

--- src/main/java/volume/four/ExerciseSeventeen.java
+++ src/main/java/volume/four/ExerciseSeventeen.java
@@ -2,13 +2,18 @@ package volume.four;
 //Exercise 17:Create a class with a constructor that takes a String argument. During construction, print the argument.
 // Create an array of object references to this class, but don’t actually create objects to assign into the array.
 // When you run the program, notice whether the initialization messages from the constructor calls are printed.
+//Exercise 18: Complete the previous exercise by creating objects to attach to the array of references.
 public class ExerciseSeventeen {
     ExerciseSeventeen(String s){
         System.out.println("String:"+s);
     }
 
     public static void main(String[] args) {
-        ExerciseSeventeen [] seventeen  = new ExerciseSeventeen[20];
+        ExerciseSeventeen [] seventeen ={
+                new ExerciseSeventeen("first"),
+                new ExerciseSeventeen("second")
+        };
+
     }
 
 }
