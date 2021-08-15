e931c6c3570952b203218e9728e40b866abddfbe
Galq <galina.sansarowa@gmail.com>
2021-08-15 17:27:07 +0300

    complete exercise 2 from volume five

---
+++ src/main/java/volume/five/ExerciseTwo.java
@@ -0,0 +1,11 @@
+package volume.five;
+//Exercise 2:Take the code fragments in this section and turn them into a program, and verify that collisions do in fact occur.
+
+import java.util.*;
+
+import volume.five.ex1v5.*;
+
+public class ExerciseTwo {
+    // Vector v =new Vector(); // collision
+    java.util.Vector v = new java.util.Vector();
+}
---
+++ src/main/java/volume/five/ex1v5/Vector.java
@@ -0,0 +1,7 @@
+package volume.five.ex1v5;
+
+public class Vector {
+    public Vector() {
+        System.out.println("Vector");
+    }
+}
