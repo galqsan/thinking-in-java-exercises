ff8ed0a10e7c9f3ab9a1c38f4bab98296103337f
Galq <galina.sansarowa@gmail.com>
2021-08-15 17:18:29 +0300

    complete exercise 1 from volume five

---
+++ src/main/java/volume/five/ExerciseOne.java
@@ -0,0 +1,10 @@
+package volume.five;
+//Exercise 1: Create a class in a package. Create an instance of your class outside of that package.
+
+import volume.five.ex1v5.*;
+
+public class ExerciseOne {
+    public static void main(String[] args) {
+        ExOne exOne = new volume.five.ex1v5.ExOne();
+    }
+}
---
+++ src/main/java/volume/five/ex1v5/ExOne.java
@@ -0,0 +1,7 @@
+package volume.five.ex1v5;
+
+public class ExOne {
+    public ExOne() {
+        System.out.println("Welcome to volume five");
+    }
+}
