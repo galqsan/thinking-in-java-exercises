cd137e15d7569dd857f5a3ccd664b515f803c509
Galq <galina.sansarowa@gmail.com>
2021-08-21 22:39:26 +0300

    complete exercise 7 from volume five

---
+++ src/main/java/volume/five/ExerciseSeven.java
@@ -0,0 +1,11 @@
+package volume.five;
+//Exercise 7:Create the library according to the code fragments describing access and Widget.
+// Create a Widget in a class that is not part of the access package.
+
+import volume.five.access.Widget;
+
+public class ExerciseSeven {
+    public static void main(String[] args) {
+        Widget w = new Widget();
+    }
+}
---
+++ src/main/java/volume/five/access/Widget.java
@@ -0,0 +1,7 @@
+package volume.five.access;
+
+public class Widget {
+    public Widget() {
+        System.out.println("Widget");
+    }
+}
