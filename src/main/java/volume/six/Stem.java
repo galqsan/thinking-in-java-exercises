eeb7af2a324388a598c0db72be6cc1e7ebaabf69
Galq <galina.sansarowa@gmail.com>
2021-09-30 00:02:39 +0300

    complete exercise 10 from volume 6

--- src/main/java/volume/six/Stem.java
+++ src/main/java/volume/six/Stem.java
@@ -3,45 +3,47 @@ package volume.six;
 //Exercise 9: Create a class called Root that contains an instance of each of the classes (that you also create) named Component1, Component2, and Component3.
 // Derive a class Stem from Root that also contains an instance of each “component.”
 // All classes should have default constructors that print a message about that class.
+//Exercise 10:Modify the previous exercise so that each class only has non-default constructors.
 class Root {
 
-    Component1 component1 = new Component1();
-    Component2 component2 = new Component2();
-    Component3 component3 = new Component3();
+    Component1 component1 = new Component1('c');
+    Component2 component2 = new Component2(9);
+    Component3 component3 = new Component3(19f);
 
-    Root() {
+    Root(char a, int i) {
         System.out.println("Class Root");
     }
 }
 
 class Component1 {
-    Component1() {
+    Component1(char a) {
         System.out.println("Class Component1");
     }
 }
 
 class Component2 {
-    Component2() {
+    Component2(int i) {
         System.out.println("Class Component2");
     }
 }
 
 class Component3 {
-    Component3() {
+    Component3(float f) {
         System.out.println("Class Component3");
     }
 }
 
 public class Stem extends Root {
-    Component1 c1 = new Component1();
-    Component2 c2 = new Component2();
-    Component3 c3 = new Component3();
+    Component1 c1 = new Component1('b');
+    Component2 c2 = new Component2(89);
+    Component3 c3 = new Component3(2f);
 
-    Stem() {
+    Stem(boolean b) {
+        super('m', 9);
         System.out.println("Class Stem");
     }
 
     public static void main(String[] args) {
-        Stem s = new Stem();
+        Stem s = new Stem(true);
     }
 }
