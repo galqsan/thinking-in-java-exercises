fc02e03edb1da08a53bbc4d1e3f0c72411809804
Galq <galina.sansarowa@gmail.com>
2021-09-29 23:35:32 +0300

    complete exercise 7 from volume 6

--- src/main/java/volume/six/C.java
+++ src/main/java/volume/six/C.java
@@ -2,24 +2,30 @@ package volume.six;
 
 //Exercise 5: Create two classes, A and B, with default constructors (empty argument lists) that announce themselves. Inherit a new class called C from A, and create a member of class B inside C.
 // Do not create a constructor for C. Create an object of class C and observe the results.
+//Exercise 7: Modify Exercise 5 so that A and B have constructors with arguments instead of default constructors.
+// Write a constructor for C and perform all initialization within C’s constructor.
 class A {
-    A() {
-        System.out.println(" Class A");
+    A(int i) {
+        System.out.println(" Class A ");
     }
 
 }
 
 class B {
-    B() {
-        System.out.println(" Class B");
+    B(int i) {
+        System.out.println(" Class B ");
     }
 
 }
 
 public class C extends A {
-    static B b = new B();
+    C() {
+        super(6);
+        System.out.println(" Class C");
+    }
 
     public static void main(String[] args) {
+        B b = new B(7);
         C c = new C();
     }
 }
