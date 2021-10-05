a99620a411b64aa962355dc9140a4f2acf45dad2
Galq <galina.sansarowa@gmail.com>
2021-10-05 16:14:54 +0300

    complete exercise 17 from volume 6

--- src/main/java/volume/six/Frog.java
+++ src/main/java/volume/six/Frog.java
@@ -4,10 +4,13 @@ package volume.six;
 // From this, inherit a class called Frog.
 // Put appropriate methods in the base class.
 // In main( ), create a Frog and upcast it to Amphibian and demonstrate that all the methods still work.
+//Exercise 17: Modify Exercise 16 so that Frog overrides the method definitions from the base class (provides new definitions using the same method signatures).
+// Note what happens in main( ).
 class Amphibian {
     void live(Amphibian a) {
         System.out.println("To live Amphibian ");
         a.eat();
+        a.move();
     }
 
     void eat() {
@@ -20,12 +23,31 @@ class Amphibian {
 }
 
 public class Frog extends Amphibian {
+    @Override
+    void move() {
+        System.out.println("Frog jump");
+    }
+
+    @Override
+    void live(Amphibian a) {
+        System.out.println("To live Frog");
+        a.eat();
+        a.move();
+    }
+
+    @Override
+    void eat() {
+        System.out.println("Frog eat");
+    }
+
 
     public static void main(String[] args) {
         Frog frog = new Frog();
         Amphibian amphibian = new Amphibian();
+        frog.move();
+        frog.eat();
+        frog.live(frog);
         amphibian.live(frog);
-        amphibian.move();
-        amphibian.eat();
+
     }
 }
