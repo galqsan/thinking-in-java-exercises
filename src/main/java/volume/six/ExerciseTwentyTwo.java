47972e16718c8d62dfaac8e41b02845b7127fa73
Galq <galina.sansarowa@gmail.com>
2021-10-14 00:29:43 +0300

    complete exercise 22 from volume 6

---
+++ src/main/java/volume/six/ExerciseTwentyTwo.java
@@ -0,0 +1,10 @@
+package volume.six;
+
+//Exercise 22: Create a final class and attempt to inherit from it.
+public final class ExerciseTwentyTwo {
+    ExerciseTwentyTwo() {
+        System.out.println("ExerciseTwentyTwo constructor");
+    }
+
+}
+//class TwenryTwo extends ExerciseTwentyTwo{}//cannot be inherited, because the class is final
