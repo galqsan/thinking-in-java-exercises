a82eca3a651006c3ab93d5e3f6be26acdb08b510
Galq <galina.sansarowa@gmail.com>
2021-10-18 01:12:28 +0300

    complete exercise 5 from volume 7

--- src/main/java/volume/seven/Cycling.java
+++ src/main/java/volume/seven/Cycling.java
@@ -2,15 +2,26 @@ package volume.seven;
 
 // Exercise 1: Create a Cycle class, with subclasses Unicycle, Bicycle and Tricycle.
 // Demonstrate that an instance of each type can be upcast to Cycle via a ride( ) method.
+//Exercise 5: Starting from Exercise 1, add a wheels( ) method in Cycle, which returns the number of wheels.
+// Modify ride( ) to call wheels( ) and verify that polymorphism works.
 class Cycle {
     public String toString() {
         return "Cycle";
+    }
+
+    public int wheels() {
+        return 0;
     }
 }
 
 class Bicycle extends Cycle {
     public String toString() {
         return "Bicycle";
+    }
+
+    @Override
+    public int wheels() {
+        return 2;
     }
 }
 
@@ -19,17 +30,30 @@ class Tricycle extends Cycle {
         return "Tricycle";
     }
 
+    @Override
+    public int wheels() {
+        return 3;
+    }
+
 }
 
 class Unicycle extends Cycle {
     public String toString() {
         return "Unicycle";
     }
+
+    @Override
+    public int wheels() {
+        return 1;
+    }
 }
 
 public class Cycling {
     public static void ride(Cycle c) {
-        System.out.println(c + ".ride");
+        int i;
+        System.out.println(c + ".ride ");
+        i = c.wheels();
+        System.out.println("wheels " + i);
     }
 
     public static void main(String[] args) {
@@ -41,6 +65,7 @@ public class Cycling {
         ride(u);
         ride(b);
         ride(t);
+
 
     }
 }
