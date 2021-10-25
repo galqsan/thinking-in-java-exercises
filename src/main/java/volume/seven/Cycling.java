ca2f11aa99878d6cc281e0c4049a7b136e8f6ee0
Galq <galina.sansarowa@gmail.com>
2021-10-25 21:58:08 +0300

    complete exercise 17 from volume 7

--- src/main/java/volume/seven/Cycling.java
+++ src/main/java/volume/seven/Cycling.java
@@ -4,6 +4,10 @@ package volume.seven;
 // Demonstrate that an instance of each type can be upcast to Cycle via a ride( ) method.
 //Exercise 5: Starting from Exercise 1, add a wheels( ) method in Cycle, which returns the number of wheels.
 // Modify ride( ) to call wheels( ) and verify that polymorphism works.
+//Exercise 17:Using the Cycle hierarchy from Exercise 1, add a balance( ) method to Unicycle and Bicycle, but not to Tricycle.
+// Create instances of all three types and upcast them to an array of Cycle.
+// Try to call balance( ) on each element of the array and observe the results.
+// Downcast and call balance( ) and observe what happens.
 class Cycle {
     public String toString() {
         return "Cycle";
@@ -17,6 +21,10 @@ class Cycle {
 class Bicycle extends Cycle {
     public String toString() {
         return "Bicycle";
+    }
+
+    public void balance() {
+        System.out.println("balance");
     }
 
     @Override
@@ -42,6 +50,10 @@ class Unicycle extends Cycle {
         return "Unicycle";
     }
 
+    public void balance() {
+        System.out.println("balance");
+    }
+
     @Override
     public int wheels() {
         return 1;
@@ -57,14 +69,22 @@ public class Cycling {
     }
 
     public static void main(String[] args) {
-        Cycle c = new Cycle();
-        Unicycle u = new Unicycle();
-        Bicycle b = new Bicycle();
-        Tricycle t = new Tricycle();
-        ride(c);
-        ride(u);
-        ride(b);
-        ride(t);
+        Cycle c[] = {
+                new Cycle(),
+                new Unicycle(),
+                new Bicycle(),
+                new Tricycle()};
+
+        for (Cycle cycle : c) {
+            ride(cycle);
+
+        }
+        //method not found in Cycle:
+        //c[1].balance();
+        //c[2].balance();
+        //c[3].balance();
+        ((Unicycle) c[1]).balance();
+        ((Bicycle) c[2]).balance();
 
 
     }
