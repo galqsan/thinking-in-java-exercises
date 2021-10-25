be315682c78fc06ed5669369b0dd6f1b16956b18
Galq <galina.sansarowa@gmail.com>
2021-10-25 21:39:55 +0300

    complete exercise 12 from volume 7

--- src/main/java/volume/seven/Rodents.java
+++ src/main/java/volume/seven/Rodents.java
@@ -7,7 +7,42 @@ package volume.seven;
 // Inherit a class and override the second method.
 // Create an object of the derived class, upcast it to the base type, and call the first method.
 // Explain what happens.
+//Exercise 12:Modify Exercise 9 so that it demonstrates the order of initialization of the base classes and derived classes.
+// Now add member objects to both the base and derived classes and show the order in which their initialization occurs during construction.
+class Characteristic {
+    private String s;
+
+    Characteristic(String s) {
+        this.s = s;
+        System.out.println("Characteristic: " + s + " ");
+
+    }
+
+    public String toString() {
+        return "Characteristic ";
+    }
+
+}
+
+class Description {
+    private String s;
+
+    Description(String s, Characteristic characteristic) {
+        this.s = s;
+        System.out.println(" Description: " + s);
+
+    }
+
+    public String toString() {
+        return "Description ";
+    }
+}
+
 class Rodent {
+
+    Characteristic ch = new Characteristic("have long nails");
+    Description de = new Description("are mammals ", ch);
+
     void eat() {
         System.out.println("Rodent.eat");
     }
@@ -20,25 +55,36 @@ class Rodent {
     void move() {
         System.out.println("Rodent.move");
     }
+
+    Rodent() {
+        System.out.println("Rodent");
+
+    }
 }
 
 class Mouse extends Rodent {
+    Characteristic ch = new Characteristic("have slender tail with hair");
+    Description de = new Description(" they have 20 chromosomes ", ch);
+
     @Override
     void eat() {
         System.out.println("Mouse.eat");
+    }
+
+    Mouse() {
+        System.out.println("Mouse");
     }
 
     @Override
     void move() {
         System.out.println("Mouse.move");
     }
-
-    public String toString() {
-        return "Mouse";
-    }
 }
 
 class Gerbil extends Rodent {
+    Characteristic ch = new Characteristic("have long hind feet and fairly large ears");
+    Description de = new Description(" they build extensive networks of underground tunnels ", ch);
+
     @Override
     void eat() {
         System.out.println("Gerbil.eat");
@@ -48,9 +94,17 @@ class Gerbil extends Rodent {
     void move() {
         System.out.println("Gerbil.move");
     }
+
+    Gerbil() {
+        System.out.println("Gerbil");
+    }
 }
 
+
 class Hamster extends Rodent {
+    Characteristic ch = new Characteristic("have very short tail");
+    Description de = new Description(" they are kept as pets ", ch);
+
     @Override
     void eat() {
         System.out.println("Hamster.eat");
@@ -59,6 +113,10 @@ class Hamster extends Rodent {
     @Override
     void move() {
         System.out.println("Hamster.move");
+    }
+
+    Hamster() {
+        System.out.println("Hamster");
     }
 }
 
