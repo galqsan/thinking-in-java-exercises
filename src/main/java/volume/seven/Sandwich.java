3b5b93a686ede1a9931c5525ea7bd012438027cf
Galq <galina.sansarowa@gmail.com>
2021-10-28 18:24:32 +0300

    complete exercise 7 from volume 8

--- src/main/java/volume/seven/Sandwich.java
+++ src/main/java/volume/seven/Sandwich.java
@@ -1,6 +1,15 @@
 package volume.seven;
 
 //Exercise 11: Add class Pickle to Sandwich.java.
+//Volume 8:Exercise 8:In polymorphism.Sandwich.java, create an interface called FastFood (with appropriate methods) and change Sandwich so that it also implements FastFood.
+interface FastFood {
+    void cook();
+
+    void preparation();
+
+    void plating();
+}
+
 class Meal {
     Meal() {
         System.out.println("Meal()");
@@ -43,17 +52,33 @@ class PortableLunch extends Lunch {
     }
 }
 
-public class Sandwich extends PortableLunch {
+public class Sandwich extends PortableLunch implements FastFood {
     private Bread b = new Bread();
     private Cheese c = new Cheese();
     private Lettuce l = new Lettuce();
     private Pickle p = new Pickle();
 
+    public void cook() {
+        System.out.println("cooking()");
+    }
+
+    public void preparation() {
+        System.out.println("preparation()");
+    }
+
+    public void plating() {
+        System.out.println("plating()");
+    }
+
     public Sandwich() {
         System.out.println("Sandwich()");
     }
 
-    public static void main(String[] args) {
-        new Sandwich();
+    public static void main(String[] args)
+    { Sandwich sandwich=new Sandwich();
+    sandwich.preparation();
+    sandwich.cook();
+    sandwich.plating();
+
     }
 }
