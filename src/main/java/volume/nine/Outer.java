8d75405301e9b7c55e75ffa135cce16d7b9bae29
Galq <galina.sansarowa@gmail.com>
2021-11-14 23:40:39 +0200

    complete exercise 3 from volume 9

--- src/main/java/volume/nine/Outer.java
+++ src/main/java/volume/nine/Outer.java
@@ -3,11 +3,22 @@ package volume.nine;
 //Exercise 1:Write a class named Outer that contains an inner class named Inner.
 //Add a method to Outer that returns an object of type Inner.
 // In main( ), create and initialize a reference to an Inner.
+//Exercise 3:Modify Exercise 1 so that Outer has a private String field (initialized by the constructor), and Inner has a toString( ) that displays this field. Create an object of type Inner and display it.
 public class Outer {
+    private final String s;
+
     class Inner {
         Inner() {
             System.out.println("Inner class");
         }
+    }
+
+    public String toString() {
+        return s;
+    }
+
+    Outer(String s) {
+        this.s = s;
     }
 
     Inner getInner() {
@@ -15,7 +26,8 @@ public class Outer {
     }
 
     public static void main(String[] args) {
-        Outer o = new Outer();
+        Outer o = new Outer("Exercise three");
         Outer.Inner i = o.getInner();
+        System.out.println(i.toString());
     }
 }
