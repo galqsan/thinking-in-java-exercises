a29ca1bddc13c3a9b16e176af6ad19c8d961febf
Galq <galina.sansarowa@gmail.com>
2021-11-17 00:11:47 +0200

    complete exercise 10 from volume 9

--- src/main/java/volume/nine/ExerciseNine.java
+++ src/main/java/volume/nine/ExerciseNine.java
@@ -1,19 +1,22 @@
 package volume.nine;
 
 //Exercise 9:Create an interface with at least one method, and implement that interface by defining an inner class within a method, which returns a reference to your interface.
+//Exercise 10:Repeat the previous exercise but define the inner class within a scope within a method.
 interface Interface9 {
     void innerClassMethod();
 }
 
 public class ExerciseNine {
     public Interface9 innerClassMethod() {
-        class InnerClass9 implements Interface9 {
-            @Override
-            public void innerClassMethod() {
-                System.out.println("implementation of the interface method");
+        {
+            class InnerClass9 implements Interface9 {
+                @Override
+                public void innerClassMethod() {
+                    System.out.println("implementation of the interface method");
+                }
             }
+            return new InnerClass9();
         }
-        return new InnerClass9();
     }
 
     public static void main(String[] args) {
