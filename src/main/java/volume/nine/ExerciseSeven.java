bd7d4af485fde753f533da0cbdd751df52348dd0
Galq <galina.sansarowa@gmail.com>
2021-11-20 22:54:57 +0200

    complete exercise 12 from volume 9

--- src/main/java/volume/nine/ExerciseSeven.java
+++ src/main/java/volume/nine/ExerciseSeven.java
@@ -3,34 +3,47 @@ package volume.nine;
 //Exercise 7:Create a class with a private field and a private method. Create an inner class with a method that modifies the outer-class field and calls the outer-class method.
 // In a second outer-class method, create an object of the inner class and call its method, then show the effect on the outer-class object.
 //Exercise 8:Determine whether an outer class has access to the private elements of its inner class.
+//Exercise 12: Repeat Exercise 7 using an anonymous inner class.
+interface AnonymousInner {
+    void modifyPrivateField();
+
+    int h();
+}
+
 public class ExerciseSeven {
     private String s;
+    AnonymousInner innerr = anonymousInner();
 
     private void privateMethod() {
         System.out.println(s);
 
     }
 
-    class Inner7 {
-        private int i = 17;
+    public AnonymousInner anonymousInner() {
+        return new AnonymousInner() {
 
-        public void modifyPrivateField() {
-            s = "Exercise seven";
-            privateMethod();
-        }
+            private int i = 17;
+
+            public int h() {
+                return i;
+            }
+
+            public void modifyPrivateField() {
+                s = "Exercise seven";
+                privateMethod();
+
+            }
+        };
     }
 
     void outerMethod() {
-        new Inner7().modifyPrivateField();
+        innerr.modifyPrivateField();
     }
 
     public static void main(String[] args) {
         ExerciseSeven seven = new ExerciseSeven();
-        ExerciseSeven.Inner7 inner = seven.new Inner7();
+        AnonymousInner inner = seven.anonymousInner();
         seven.outerMethod();
-        System.out.println("Private inner field i=" + inner.i);
-        //The outer class has a access to the private inner class field
-
-
+        System.out.println("Private inner field i=" + inner.h());
     }
 }
