57ce04178489e7270ef9f503786f337b604b2bf3
Galq <galina.sansarowa@gmail.com>
2021-12-04 00:28:27 +0200

    complete exercise 1 from volume 10

---
+++ src/main/java/volume/ten/exerciseOne/Gerbil.java
@@ -0,0 +1,28 @@
+package volume.ten.exerciseOne;
+
+import java.util.ArrayList;
+
+//Exercise 1:Create a new class called Gerbil with an int gerbilNumber that’s initialized in the constructor.
+// Give it a method called hop( ) that displays which gerbil number this is, and that it’s hopping.
+// Create an ArrayList and add Gerbil objects to the List.
+// Now use the get( ) method to move through the List and call hop( ) for each Gerbil.
+public class Gerbil {
+    int gerbilNumber;
+
+    Gerbil(int num) {
+        this.gerbilNumber = num;
+    }
+
+    void hop() {
+        System.out.println("Gerbil " + gerbilNumber + " is hopping");
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Gerbil> gerbilList = new ArrayList<Gerbil>();
+        for (int i = 0; i < 4; i++)
+            gerbilList.add(new Gerbil(i));
+        for (int i = 0; i < gerbilList.size(); i++) {
+            gerbilList.get(i).hop();
+        }
+    }
+}
