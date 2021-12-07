ddc257fbca3d4f5a4f396286781f16b0aa0e8e85
Galq <galina.sansarowa@gmail.com>
2021-12-07 23:09:06 +0200

    complete exercise 6 from volume 10

--- src/main/java/volume/ten/exerciseFive/ListFeatures.java
+++ src/main/java/volume/ten/exerciseFive/ListFeatures.java
@@ -3,13 +3,14 @@ package volume.ten.exerciseFive;
 import java.util.*;
 
 //Exercise 5:Modify ListFeatures.java so that it uses Integers (remember autoboxing!) instead of Pets, and explain any difference in results.
+//Exercise 6:Modify ListFeatures.java so that it uses Strings instead of Pets, and explain any difference in results.
 public class ListFeatures {
-    static Random rand = new Random();
-    private ArrayList<Integer> arrayList = new ArrayList<Integer>();
+    private static Random rand = new Random();
+    private ArrayList<String> arrayList = new ArrayList<>();
 
-    ArrayList intFill(int p) {
+    private ArrayList<String> intFill(int p) {
         for (int i = 0; i < p; i++) {
-            arrayList.add(rand.nextInt(100));
+            arrayList.add(Integer.toString(rand.nextInt(100)));
         }
         return arrayList;
     }
@@ -17,25 +18,25 @@ public class ListFeatures {
     public static void main(String[] args) {
         ListFeatures listFeatures = new ListFeatures();
 
-        List<Integer> nums = listFeatures.intFill(7);
+        List<String> nums = listFeatures.intFill(7);
         System.out.println("1: " + nums);
-        Integer h = new Integer(8);
+        String h = "hamster";
         nums.add(h); // Automatically resizes
         System.out.println("2: " + nums);
         System.out.println("3: " + nums.contains(h));
         nums.remove(h); // Remove by object
-        Integer p = nums.get(2);
+        String p = nums.get(2);
         System.out.println("4: " + p + " " + nums.indexOf(p));
-        nums.add(0, 5);
-        Integer cymric = new Integer(5);
+        nums.add(0, "cymric");
+        String cymric = "cymric";
         System.out.println("5: " + nums.indexOf(cymric));
         System.out.println("6: " + nums.remove(cymric));
-// Integers are equal if the contents of two Integers are identical
+// Strings are equal if the contents of two Strings are identical
         System.out.println("7: " + nums.remove(p));
         System.out.println("8: " + nums);
-        nums.add(3, new Integer(11)); // Insert at an index
+        nums.add(3, "ni"); // Insert at an index
         System.out.println("9: " + nums);
-        List<Integer> sub = nums.subList(1, 4);
+        List<String> sub = nums.subList(1, 4);
         System.out.println("subList: " + sub);
         System.out.println("10: " + nums.containsAll(sub));
         Collections.sort(sub); // In-place sort
@@ -45,17 +46,17 @@ public class ListFeatures {
         Collections.shuffle(sub, rand); // Mix it up
         System.out.println("shuffled subList: " + sub);
         System.out.println("12: " + nums.containsAll(sub));
-        List<Integer> copy = new ArrayList<Integer>(nums);
+        List<String> copy = new ArrayList<>(nums);
         sub = Arrays.asList(nums.get(1), nums.get(4));
         System.out.println("sub: " + sub);
         copy.retainAll(sub);
         System.out.println("13: " + copy);
-        copy = new ArrayList<Integer>(nums); // Get a fresh copy
+        copy = new ArrayList<>(nums); // Get a fresh copy
         copy.remove(2); // Remove by index
         System.out.println("14: " + copy);
         copy.removeAll(sub); // Only removes exact objects
         System.out.println("15: " + copy);
-        copy.set(1, new Integer(100)); // Replace an element
+        copy.set(1, "100"); // Replace an element
         System.out.println("16: " + copy);
         copy.addAll(2, sub); // Insert a list in the middle
         System.out.println("17: " + copy);
