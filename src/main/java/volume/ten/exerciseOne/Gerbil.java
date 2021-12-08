b165f568f8c5a1f161a9cd75189a8a2339c16b56
Galq <galina.sansarowa@gmail.com>
2021-12-09 01:51:51 +0200

    complete exercise 8 from volume 10

--- src/main/java/volume/ten/exerciseOne/Gerbil.java
+++ src/main/java/volume/ten/exerciseOne/Gerbil.java
@@ -1,11 +1,14 @@
 package volume.ten.exerciseOne;
 
+
 import java.util.ArrayList;
+import java.util.Iterator;
 
 //Exercise 1:Create a new class called Gerbil with an int gerbilNumber that’s initialized in the constructor.
-// Give it a method called hop( ) that displays which gerbil number this is, and that it’s hopping.
-// Create an ArrayList and add Gerbil objects to the List.
-// Now use the get( ) method to move through the List and call hop( ) for each Gerbil.
+//Give it a method called hop( ) that displays which gerbil number this is, and that it’s hopping.
+//Create an ArrayList and add Gerbil objects to the List.
+//Now use the get( ) method to move through the List and call hop( ) for each Gerbil.
+//Exercise 8:Modify Exercise l so it uses an Iterator to move through the List while calling hop( ).
 public class Gerbil {
     int gerbilNumber;
 
@@ -18,11 +21,13 @@ public class Gerbil {
     }
 
     public static void main(String[] args) {
-        ArrayList<Gerbil> gerbilList = new ArrayList<Gerbil>();
+        ArrayList<Gerbil> gerbilList = new ArrayList<>();
+        Iterator<Gerbil> it = gerbilList.iterator();
         for (int i = 0; i < 4; i++)
             gerbilList.add(new Gerbil(i));
-        for (int i = 0; i < gerbilList.size(); i++) {
-            gerbilList.get(i).hop();
+        while (it.hasNext()) {
+            it.next().hop();
         }
+
     }
 }
