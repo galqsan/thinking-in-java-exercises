f63120f63553d99bb2003c388657db937684ce7b
Galq <galina.sansarowa@gmail.com>
2021-12-13 22:48:56 +0200

    complete exercise 13 from volume 10

--- src/main/java/volume/nine/exerciseTwentyFour/Controller.java
+++ src/main/java/volume/nine/exerciseTwentyFour/Controller.java
@@ -1,25 +1,33 @@
 package volume.nine.exerciseTwentyFour;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
+//Volume 10:Exercise 13:In the innerclasses/GreenhouseController.java example, the class Controller uses an ArrayList.
+// Change the code to use a LinkedList instead, and use an Iterator to cycle through the set of events.
 public class Controller {
     // A class from java.util to hold Event objects:
-    private List<Event> eventList = new ArrayList<Event>();
+    private List<Event> eventList = new LinkedList<>();
 
     public void addEvent(Event c) {
         eventList.add(c);
     }
 
     public void run() {
-        while (eventList.size() > 0)
+        while (eventList.size() > 0) {
 // Make a copy so you’re not modifying the list
 // while you’re selecting the elements in it:
-            for (Event e : new ArrayList<Event>(eventList))
+            Iterator<Event> it = new LinkedList<>(eventList).iterator();
+            while (it.hasNext()) {
+                Event e = it.next();
                 if (e.ready()) {
                     System.out.println(e);
                     e.action();
-                    eventList.remove(e);
+                    it.remove();
                 }
+            }
+        }
     }
 }
