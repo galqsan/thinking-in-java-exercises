253eec5e9e4ab9e3281a82231255d5433c4e468c
Galq <galina.sansarowa@gmail.com>
2021-12-19 21:03:11 +0200

    complete exercise 17 from volume 10

--- src/main/java/volume/ten/exerciseOne/Gerbil.java
+++ src/main/java/volume/ten/exerciseOne/Gerbil.java
@@ -2,13 +2,17 @@ package volume.ten.exerciseOne;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
 //Exercise 1:Create a new class called Gerbil with an int gerbilNumber that’s initialized in the constructor.
 //Give it a method called hop( ) that displays which gerbil number this is, and that it’s hopping.
 //Create an ArrayList and add Gerbil objects to the List.
 //Now use the get( ) method to move through the List and call hop( ) for each Gerbil.
 //Exercise 8:Modify Exercise l so it uses an Iterator to move through the List while calling hop( ).
+//Exercise 17:Take the Gerbil class in Exercise 1 and put it into a Map instead, associating each Gerbil’s name (e.g. "Fuzzy" or "Spot") as a String (the key) for each Gerbil (the value) you put in the table.
+// Get an Iterator for the keySet( ) and use it to move through the Map, looking up the Gerbil for each key and printing out the key and telling the Gerbil to hop( ).
 public class Gerbil {
     int gerbilNumber;
 
@@ -16,17 +20,25 @@ public class Gerbil {
         this.gerbilNumber = num;
     }
 
+    public String toString() {
+        return "Gerbil ";
+    }
+
     void hop() {
         System.out.println("Gerbil " + gerbilNumber + " is hopping");
     }
 
     public static void main(String[] args) {
-        ArrayList<Gerbil> gerbilList = new ArrayList<>();
-        Iterator<Gerbil> it = gerbilList.iterator();
-        for (int i = 0; i < 4; i++)
-            gerbilList.add(new Gerbil(i));
+        Map<String, Gerbil> gerbilList = new HashMap<>();
+        gerbilList.put("Fuzzy", new Gerbil(1));
+        gerbilList.put("Spot", new Gerbil(2));
+        gerbilList.put("Perry", new Gerbil(3));
+        gerbilList.put("Garfield", new Gerbil(4));
+        Iterator<String> it = gerbilList.keySet().iterator();
         while (it.hasNext()) {
-            it.next().hop();
+            String s = it.next();
+            System.out.print(s + " :");
+            gerbilList.get(s).hop();
         }
 
     }
