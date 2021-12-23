fe0b7bcc755f76415a9d2f11baefce270cacf853
Galq <galina.sansarowa@gmail.com>
2021-12-23 20:45:26 +0200

    complete exercise 20 from volume 10

--- src/main/java/volume/ten/ExerciseSixteen.java
+++ src/main/java/volume/ten/ExerciseSixteen.java
@@ -6,18 +6,24 @@ import java.util.*;
 
 //Exercise 16:Create a Set of the vowels.
 // Working from UniqueWords.Java, count and display the number of vowels in each input word, and also display the total number of vowels in the input file.
+//Exercise 20:Modify Exercise 16 so that you keep a count of the occurrence of each vowel.
 public class ExerciseSixteen {
     private static Set<Character> characterSet = new HashSet<>(Arrays.asList('a', 'e', 'o', 'u',
             'i', 'A', 'E', 'O', 'U', 'I'));
 
     public static void main(String[] args) {
         Set<String> words = new HashSet<>();
-        int countWord;
+        Map<Character, Integer> map = new HashMap<>();
+        Integer countVowels;
         int countFile = 0;
-        for (String w : new TextFile("C:\\Repos\\ThinkingInJava\\thinking-in-java-exercises\\src\\main\\java\\volume\\ten\\ExerciseSixteen.java", "\\W+")) {
-            countWord = 0;
+        String fileName = "C:\\Repos\\ThinkingInJava\\thinking-in-java-exercises\\src\\main\\java\\volume\\ten\\ExerciseSixteen.java";
+        String splitter = "\\W+";
+        for (String w : new TextFile(fileName, splitter)) {
+            int countWord = 0;
             for (char c : w.toCharArray()) {
                 if (characterSet.contains(c)) {
+                    countVowels = map.get(c);
+                    map.put(c, countVowels == null ? 1 : countVowels + 1);
                     countWord++;
                 }
                 if (!words.contains(w)) {
@@ -27,6 +33,7 @@ public class ExerciseSixteen {
                 countFile += countWord;
             }
         }
+        System.out.println(map);
         System.out.println("file vowels " + countFile);
     }
 }
