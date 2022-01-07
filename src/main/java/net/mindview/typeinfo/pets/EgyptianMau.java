28738aacbe8e683f1e10b5d92cbd22dbf83765c2
Galq <galina.sansarowa@gmail.com>
2022-01-07 18:47:19 +0200

    files from the book

---
+++ src/main/java/net/mindview/typeinfo/pets/EgyptianMau.java
@@ -0,0 +1,13 @@
+package net.mindview.typeinfo.pets;
+
+//: typeinfo/pets/EgyptianMau.java
+public class EgyptianMau extends Cat {
+	public EgyptianMau(String name) {
+		super(name);
+	}
+
+	public EgyptianMau() {
+		super();
+	}
+} ///:~
+
---
+++ src/main/java/net/mindview/typeinfo/pets/Person.java
@@ -0,0 +1,8 @@
+package net.mindview.typeinfo.pets;
+
+//: typeinfo/pets/Person.java 
+public class Person extends Individual {
+	public Person(String name) {
+		super(name);
+	}
+} ///:~
