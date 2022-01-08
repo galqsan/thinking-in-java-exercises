31b798faa29a457ff50f5bead50f337b4ba21306
Galq <galina.sansarowa@gmail.com>
2022-01-08 23:16:49 +0200

    update exercise 30 from volume 10

--- src/main/java/volume/ten/exerciseThirty/CollectionSequence.java
+++ src/main/java/volume/ten/exerciseThirty/CollectionSequence.java
@@ -8,21 +8,6 @@ import java.util.Collection;
 import java.util.Iterator;
 
 //Exercise 30:Modify CollectionSequence.java so that it does not inherit from AbstractCollection, but instead implements Collection.
-class InterfaceVsIterator {
-    public static void display(Iterator<Pet> it) {
-        while (it.hasNext()) {
-            Pet p = it.next();
-            System.out.print(p.id() + ":" + p + " ");
-        }
-        System.out.println();
-    }
-
-    public static void display(Collection<Pet> pets) {
-        for (Pet p : pets)
-            System.out.print(p.id() + ":" + p + " ");
-        System.out.println();
-    }
-}
 
 public class CollectionSequence
         implements Collection<Pet> {
---
+++ src/main/java/volume/ten/exerciseThirty/InterfaceVsIterator.java
@@ -0,0 +1,23 @@
+package volume.ten.exerciseThirty;
+
+import net.mindview.typeinfo.pets.Pet;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+public class InterfaceVsIterator {
+    public static void display(Iterator<Pet> it) {
+        while (it.hasNext()) {
+            Pet p = it.next();
+            System.out.print(p.id() + ":" + p + " ");
+        }
+        System.out.println();
+    }
+
+    public static void display(Collection<Pet> pets) {
+        for (Pet p : pets)
+            System.out.print(p.id() + ":" + p + " ");
+        System.out.println();
+    }
+}
+
