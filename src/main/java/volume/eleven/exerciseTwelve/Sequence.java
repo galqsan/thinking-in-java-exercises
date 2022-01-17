922e0af8cee84743d2d4d5f701a9216f63e8673e
Galq <galina.sansarowa@gmail.com>
2022-01-17 17:28:46 +0200

    complete exercise 12 from volume 11

---
+++ src/main/java/volume/eleven/exerciseTwelve/Sequence.java
@@ -0,0 +1,59 @@
+package volume.eleven.exerciseTwelve;
+
+//Exercise 12:Modify innerclasses/Sequence.java so that it throws an appropriate exception if you try to put in too many elements.
+interface Selector {
+    boolean end();
+
+    Object current();
+
+    void next();
+}
+
+public class Sequence {
+    private Object[] items;
+    private int next = 0;
+
+    public Sequence(int size) {
+        items = new Object[size];
+    }
+
+    public void add(Object x) {
+        if (next < items.length) {
+            items[next++] = x;
+        } else {
+            throw new IndexOutOfBoundsException();
+        }
+
+    }
+
+    private class SequenceSelector implements Selector {
+        private int i = 0;
+
+        public boolean end() {
+            return i == items.length;
+        }
+
+        public Object current() {
+            return items[i];
+        }
+
+        public void next() {
+            if (i < items.length) i++;
+        }
+    }
+
+    public Selector selector() {
+        return new SequenceSelector();
+    }
+
+    public static void main(String[] args) {
+        Sequence sequence = new Sequence(10);
+        for (int i = 0; i < 10; i++)
+            sequence.add(Integer.toString(i));
+        Selector selector = sequence.selector();
+        while (!selector.end()) {
+            System.out.print(selector.current() + " ");
+            selector.next();
+        }
+    }
+}
