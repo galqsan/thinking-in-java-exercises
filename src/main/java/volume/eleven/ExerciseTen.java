a975a8ee74a35fec9c5eec2984089c2d01cd3db1
Galq <galina.sansarowa@gmail.com>
2022-01-17 14:30:06 +0200

    complete exercise 11 from volume 11

--- src/main/java/volume/eleven/ExerciseTen.java
+++ src/main/java/volume/eleven/ExerciseTen.java
@@ -3,14 +3,9 @@ package volume.eleven;
 //Exercise 10: Create a class with two methods, f( ) and g( ).
 // In g( ), throw an exception of a new type that you define. In f( ), call g( ), catch its exception and, in the catch clause, throw a different exception (of a second type that you define).
 // Test your code in main( ).
+//Exercise 11:Repeat the previous exercise, but inside the catch clause, wrap g( )’s exception in a RuntimeException.
 class OneException extends Exception {
     OneException(String s) {
-        super(s);
-    }
-}
-
-class TwoExceptions extends Exception {
-    TwoExceptions(String s) {
         super(s);
     }
 }
@@ -20,21 +15,16 @@ public class ExerciseTen {
         throw new OneException("Inside g()");
     }
 
-    public static void f() throws TwoExceptions {
+    public static void f() {
         try {
             g();
         } catch (OneException e) {
             e.printStackTrace(System.out);
-            throw new TwoExceptions("Inside f()");
+            throw new RuntimeException(e);
         }
     }
 
     public static void main(String[] args) {
-        try {
-            f();
-        } catch (TwoExceptions twoExceptions) {
-            System.out.println("In main:");
-            twoExceptions.printStackTrace(System.out);
-        }
+        f();
     }
 }
