adc35d536b3de320b366f3a6991bc9e65727a4ff
galqsan <galina.sansarowa@gmail.com>
2022-01-30 16:14:15 +0200

    fix comment quotes

--- src/main/java/volume/two/Dog.java
+++ src/main/java/volume/two/Dog.java
@@ -1,6 +1,6 @@
 package volume.two;
 // Exercise 5 :  Create a class called Dog containing two Strings: name and says.
-// In main( ), create two dog objects with names “spot” (who says, “Ruff!”) and “scruffy” (who says, “Wurf!”).
+// In main( ), create two dog objects with names "spot" (who says, "Ruff!") and "scruffy" (who says, "Wurf!").
 // Then display their names and what they say.
 //Exercise 6:  Following Exercise 5, create a new Dog reference and assign it to spot’s object.
 // Test for comparison using == and equals( ) for all references.
