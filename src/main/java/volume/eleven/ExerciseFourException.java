f0d3bf9241485f6bb4dd5656582ad85d57e6bd3b
galqsan <galina.sansarowa@gmail.com>
2022-01-30 12:14:05 +0200

    complete exercise 28 from volume 11

--- src/main/java/volume/eleven/ExerciseFourException.java
+++ src/main/java/volume/eleven/ExerciseFourException.java
@@ -4,7 +4,9 @@ package volume.eleven;
 // Write a constructor for this class that takes a String argument and stores it inside the object with a String reference.
 // Write a method that displays the stored String.
 // Create a try-catch clause to exercise your new exception.
-public class ExerciseFourException extends Exception {
+//Exercise 28: Modify Exercise 4 so that the custom exception class inherits from
+//RuntimeException, and show that the compiler allows you to leave out the try block.
+public class ExerciseFourException extends RuntimeException {
     private String s;
 
     ExerciseFourException(String s) {
@@ -18,11 +20,7 @@ public class ExerciseFourException extends Exception {
     }
 
     public static void main(String[] args) {
-        try {
-            throw new ExerciseFourException("throw ExerciseFourException ");
-        } catch (ExerciseFourException e) {
-            e.printS();
-        }
+        throw new ExerciseFourException("throw ExerciseFourException ");
 
     }
 }
