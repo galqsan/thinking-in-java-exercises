8f4ebe5019dc03a70187301b118057fb600907ae
galqsan <galina.sansarowa@gmail.com>
2022-04-07 22:42:17 +0300

    complete exercise 5 from volume 13

--- src/main/java/volume/thirteen/exerciseThree/Shapes.java
+++ src/main/java/volume/thirteen/exerciseThree/Shapes.java
@@ -1,5 +1,6 @@
 package volume.thirteen.exerciseThree;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,27 +9,33 @@ Shape, then downcast it back to a Rhomboid. Try down casting to a Circle and see
 happens.*/
 /*Exercise 4: Modify the previous exercise so that it uses instanceof to check the type
 before performing the downcast. */
+/*Exercise 5:Implement a rotate(Shape) method in Shapes.java, such that it
+checks to see if it is rotating a Circle (and, if so, doesn't perform the operation).*/
 abstract class Shape {
+    boolean highlight = false;
+
     void draw() {
         System.out.println(this + ".draw()");
     }
-
-    abstract public String toString();
+    public abstract String toString();
 }
 
 class Circle extends Shape {
+
     public String toString() {
         return "Circle";
     }
 }
 
 class Square extends Shape {
+
     public String toString() {
         return "Square";
     }
 }
 
 class Triangle extends Shape {
+
     public String toString() {
         return "Triangle";
     }
@@ -42,12 +49,19 @@ class Rhomboid extends Shape {
 }
 
 public class Shapes {
+    static void rotate(Shape s) {
+        if (!(s instanceof Circle)) {
+            System.out.println("rotate " + s);
+        }
+    }
+
     public static void main(String[] args) {
         List<Shape> shapeList = Arrays.asList(
                 new Circle(), new Square(), new Triangle(), new Rhomboid()
         );
         for (Shape shape : shapeList) {
             shape.draw();
+            rotate(shape);
         }
 
         Shape s = new Rhomboid();
@@ -60,5 +74,6 @@ public class Shapes {
         } else {
             System.out.println(" s is not a Circle");
         }
+
     }
 }
