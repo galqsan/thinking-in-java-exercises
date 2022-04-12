868c98dc12b0bbdfab7ca699cb3a1fd125384fa2
galqsan <galina.sansarowa@gmail.com>
2022-04-12 21:16:37 +0300

    complete exercise 6 from volume 13

--- src/main/java/volume/thirteen/exerciseThree/Shapes.java
+++ src/main/java/volume/thirteen/exerciseThree/Shapes.java
@@ -11,41 +11,54 @@ happens.*/
 before performing the downcast. */
 /*Exercise 5:Implement a rotate(Shape) method in Shapes.java, such that it
 checks to see if it is rotating a Circle (and, if so, doesn't perform the operation).*/
+/*Exercise 6: Modify Shapes.java so that it can "highlight" (set a flag in) all shapes of
+a particular type. The toString( ) method for each derived Shape should indicate whether
+that Shape is "highlighted." */
 abstract class Shape {
     boolean highlight = false;
 
     void draw() {
         System.out.println(this + ".draw()");
     }
-    public abstract String toString();
+
+    public String toString() {
+        return getClass().getName() + (highlight ? " Highlighted" : " Normal");
+    }
+
+    static List<Shape> shapes = new ArrayList<>();
+
+    Shape() {
+        shapes.add(this);
+    }
+
+    static void highlighting(Class<?> type) {
+        for (Shape shape : shapes) {
+            if (type.isInstance(shape)) {
+                shape.highlight = true;
+            }
+        }
+    }
+
+    static void clearHighlight(Class<?> type) {
+        for (Shape shape : shapes) {
+            if (type.isInstance(shape)) {
+                shape.highlight = false;
+            }
+        }
+    }
+
 }
 
 class Circle extends Shape {
-
-    public String toString() {
-        return "Circle";
-    }
 }
 
 class Square extends Shape {
-
-    public String toString() {
-        return "Square";
-    }
 }
 
 class Triangle extends Shape {
-
-    public String toString() {
-        return "Triangle";
-    }
 }
 
 class Rhomboid extends Shape {
-
-    public String toString() {
-        return "Rhomboid";
-    }
 }
 
 public class Shapes {
@@ -59,10 +72,14 @@ public class Shapes {
         List<Shape> shapeList = Arrays.asList(
                 new Circle(), new Square(), new Triangle(), new Rhomboid()
         );
+        Shape.highlighting(Rhomboid.class);
         for (Shape shape : shapeList) {
             shape.draw();
             rotate(shape);
         }
+        Shape.clearHighlight(Rhomboid.class);
+        //checking whether the Rhomboid is still highlighted
+        shapeList.get(3).draw();
 
         Shape s = new Rhomboid();
         s.draw();
