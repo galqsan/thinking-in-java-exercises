cc6fe18fab9b0693ffd122f548e255797a46be98
galqsan <galina.sansarowa@gmail.com>
2022-04-26 20:06:45 +0300

    complete exercise 9 from volume 13

--- src/main/java/volume/thirteen/ExerciseEight.java
+++ src/main/java/volume/thirteen/ExerciseEight.java
@@ -2,11 +2,20 @@ package volume.thirteen;
 
 import volume.eight.ExThirteen;
 
+import java.lang.reflect.Field;
+
 /*Exercise 8:  Write a method that takes an object and recursively prints all the classes
 in that object’s hierarchy. */
+/*Exercise 9: Modify the previous exercise so that it uses
+Class.getDeclaredFields( ) to also display information about the fields in a class.*/
 public class ExerciseEight {
     public static void hierarchyPrinter(Class c) {
         if (c == null) return;
+        Class cls = c.getClass();
+        Field[] fields = cls.getDeclaredFields();
+        for (int i = 0; i < fields.length; i++) {
+            System.out.println("Field = " + fields[i].toString());
+        }
         System.out.println(c.getSimpleName());
         for (Class i : c.getInterfaces()) {
             System.out.println("Interfaces hierarchy: " + i.getSimpleName());
